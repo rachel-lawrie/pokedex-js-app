5e8df84a843c9db24a65ffc9390cbbb84e372efa
rachel-lawrie <124215524+rachel-lawrie@users.noreply.github.com>
2023-02-20 15:51:20 -0800

    Created test array of pokemon objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
-alert("Hello world");
+//create pokemon objects
+let pokemon1 = {
+  name: "charmander",
+  height: 0.6,
+  type: "fire",
+};
+let pokemon2 = {
+  name: "arbok",
+  height: 3.5,
+  type: "poison",
+};
+let pokemon3 = {
+  name: "jigglypuff",
+  height: 0.5,
+  type: "fairy",
+};
+
+//create array filled with the pokemon objects
+let pokemonList = [pokemon1, pokemon2, pokemon3];
