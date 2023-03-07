bcb000e220f27f71e4226f9d46f0da3214bf14bc
rachel-lawrie <124215524+rachel-lawrie@users.noreply.github.com>
2023-03-06 20:00:40 -0800

    When customer clicks pokemon, see modal pupulated w/ height + picture
    Working on bug inhibiting populating types

--- js/scripts.js
+++ js/scripts.js
@@ -11,14 +11,30 @@ let pokemonRepository = (function () {
     pokemonList.push(pokemon);
   }
 
-  // function showDetails(pokemon) {
-  //   loadDetails(pokemon).then(function () {
-  //     showModal();
-  //     //create modal to display pokemon details (name, height, img and types) to user when clicked
+  //create modal to display pokemon details (name, height, img and types) to user when clicked
+  function showDetails(pokemon) {
+    loadDetails(pokemon).then(function () {
+      function populateTypes(pokemon) {
+        let typesarray = [];
+        var types = pokemon.types[i];
+        for (var i = 0; i < pokemon.types.length; i++) {
+          typesarray.push(i);
+          console.log(typesarray);
+        }
+      }
 
-  //     //console.log(pokemon);
-  //   });
-  // }
+      let details = "height: " + pokemon.height + "\r\n" + "types: ";
+      let imagelink = pokemon.imageUrl;
+      showModal(pokemon.name, details, imagelink);
+      // want to make image appear rather than URL
+      // also want to include the types
+
+      //old code that console logged the pokemon details
+      // function showDetails(pokemon) {
+      //   loadDetails(pokemon).then(function () {
+      // console.log(pokemon);
+    });
+  }
 
   function addListItem(pokemon) {
     let pokemonListItems = document.querySelector(".pokemon-list");
@@ -30,9 +46,8 @@ let pokemonRepository = (function () {
     pokemonListItems.appendChild(listItem);
     //log pokemon details when clicked
     button.addEventListener("click", function (event) {
-      showModal();
+      showDetails(pokemon);
     });
-    //showDetails(pokemon);
   }
 
   //fetch name and url from api, and push pokemon to pokemonList
@@ -73,12 +88,19 @@ let pokemonRepository = (function () {
   }
 
   // add class when clicked to make modal visible
-  function showModal(title, text) {
+  function showModal(title, text, imagelink) {
     let modalContainer = document.querySelector("#modal-container");
+
+    modalContainer.innerHTML = "";
 
     //add content
     let modal = document.createElement("div");
     modal.classList.add("modal");
+    // add image
+    let pokemonImage = document.createElement("img");
+    pokemonImage.setAttribute("id", "pokemon-image");
+    pokemonImage.src = imagelink;
+    //add close button
     let closeButtonElement = document.createElement("button");
     closeButtonElement.classList.add("modal-close");
     closeButtonElement.innerText = "X";
@@ -90,12 +112,23 @@ let pokemonRepository = (function () {
     let contentElement = document.createElement("p");
     contentElement.innerText = text;
 
+    //added line may need to delete
+    modal.appendChild(pokemonImage);
+    //continue here
     modal.appendChild(closeButtonElement);
     modal.appendChild(titleElement);
     modal.appendChild(contentElement);
     modalContainer.appendChild(modal);
 
     modalContainer.classList.add("is-visible");
+
+    modalContainer.addEventListener("click", (e) => {
+      // close modal if user clicks outside of modal (on modalContainer)
+      let target = e.target;
+      if (target === modalContainer) {
+        hideModal();
+      }
+    });
   }
 
   function hideModal() {
@@ -116,7 +149,7 @@ let pokemonRepository = (function () {
     addListItem: addListItem,
     // showDetails: showDetails,
     loadList: loadList,
-    // loadDetails: loadDetails,
+    loadDetails: loadDetails,
   };
 })();
 
